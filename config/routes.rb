817ae2df4970b416076476df50592b187df8d376
rails <rails@rails-dev.(none)>
2013-12-29 22:25:26 +0530

    login and logout link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Xpenditure::Application.routes.draw do
   get "welcome/index"
-  devise_for :users
+  devise_for :users , :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
+  
   #resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
