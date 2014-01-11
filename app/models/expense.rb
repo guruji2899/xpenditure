e367c0850d93bc1c8000a891c5cefe878fcce4fe
rails <rails@rails-dev.(none)>
2014-01-11 23:54:05 +0530

    set current month

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,10 @@
 class Expense < ActiveRecord::Base
  belongs_to :user
  belongs_to :category
- 
+
+ scope :created_between, lambda {|start_date, end_date| where("date >= ? AND date <= ?", start_date, end_date )}
+
+ def self.current_month
+   created_between(Time.now.beginning_of_month, Time.now.end_of_month).sum(:amount)
+ end
 end
